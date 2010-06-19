928cbfbd03271cc4321059c0c49b70730f26314e
Chas <chas@chas.io>
2010-06-19 12:42:49 -0700

    parse_source cleaned up

--- parse_source.py
+++ parse_source.py
@@ -1,44 +1,43 @@
 from Tkinter import *
 from OrderedDict import OrderedDict
 
-raw_source='example_annotated_code.py'
+class AnnotationReader(object):
+    def __init__(self, raw_source):
+        self.source=OrderedDict()
+        self.annotations=OrderedDict()
 
-def parse_source(raw_source):
-    '''This takes a source file with annotations in it and returns two ordered
-    dictionaries: the first maps line numbers to the source lines, the second
-    maps line numbers to annotation lines. By changing this function, you can
-    change the file format.'''
+        self.source, self.annotations = self.parse_source(raw_source)
 
-    source=OrderedDict()
-    annotations=OrderedDict()
-    in_annotations=False
-    i=1
+    def parse_source(self, raw_source):
+        '''This takes a source file with annotations in it and returns two
+        ordered dictionaries: the first maps line numbers to the source lines,
+        the second maps line numbers to annotation lines. If there is no
+        annotation for a line, it is mapped as 'lineno':'\n'. By changing this
+        function, you can change the file format.'''
 
-    for line in open(raw_source):
-        if line == "'''ANNOTATIONS\n":
-            in_annotations=True
-        elif in_annotations:
-            if line == "'''\n":
-                pass
+        source=OrderedDict()
+        annotations=OrderedDict()
+        in_annotations=False
+        i=1
+
+        for line in open(raw_source):
+            if line == "'''ANNOTATIONS\n":
+                in_annotations=True
+            elif in_annotations:
+                if line == "'''\n":
+                    pass
+                else:
+                    parsed_anno=line.split(':')
+                    annotations[parsed_anno[0]]=parsed_anno[1]
             else:
-                parsed_anno=line.split(':')
-                annotations[parsed_anno[0]]=parsed_anno[1]
-        else:
-            source[str(i)]=line
-            i += 1
-    return source, annotations
- 
-source=OrderedDict()
-annotations=OrderedDict()
-source, annotations = parse_source(raw_source)
+                source[str(i)]=line
+                i += 1
 
-#make annotation dict
-annotation_dict=OrderedDict()
-for lineno in source:
-    if lineno in annotations:
-        annotation_dict[lineno]=annotations[lineno]
-    else:
-        annotation_dict[lineno]='\n'
+        for lineno in source:
+            if lineno not in annotations:
+               annotations[lineno]='\n'
+
+        return source, annotations
 
 #GUI definitions
 root=Tk()
@@ -54,9 +53,10 @@ source_text.pack(side=LEFT)
 annotation_text.pack(side=LEFT)
 
 #import pdb; pdb.set_trace()
+source_reader=AnnotationReader('example_annotated_code.py')
 
-for lineno in source:
-    source_text.insert(INSERT, source[lineno])
-    annotation_text.insert(INSERT, annotation_dict[lineno])
+for lineno in source_reader.source:
+    source_text.insert(INSERT, source_reader.source[lineno])
+    annotation_text.insert(INSERT, source_reader.annotations[lineno])
         
 root.mainloop()
